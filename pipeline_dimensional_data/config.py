af381ad2e68bcb8042b46704430ff7c0f95f6732
nanesarukhanyan <nane_sarukhanyan@edu.aua.am>
2026-05-16 22:35:27 +0400

    Added pipeline configuration constants

--- pipeline_dimensional_data/config.py
+++ pipeline_dimensional_data/config.py
@@ -1,1 +1,18 @@
+DATABASE_NAME = "ORDER_DDS"
 
+SCHEMA_NAME = "dbo"
+
+DIMENSION_TABLES = [
+    "DimCategories",
+    "DimCustomers",
+    "DimEmployees",
+    "DimProducts",
+    "DimRegion",
+    "DimShippers",
+    "DimSuppliers",
+    "DimTerritories"
+]
+
+FACT_TABLE = "FactOrders"
+
+FACT_ERROR_TABLE = "FactOrders_Error"
